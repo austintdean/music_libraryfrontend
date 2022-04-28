179a270f3d599f9481711a0d12d78a8b9264939c
Austin <ofthevalley111@gmail.com>
2022-04-28 14:39:03 -0500

    created the components DisplayMusic and SearchBar + instantiated DisplayMusic on the App.js file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [song, setSong] = useState([{title:'Tek It', artist:'Cafune',album:'Running',release_date:'11-04-2019',genre:'Alternative',likes:0}])
+
+    return (
+    <div>
+        <DisplayMusic songEntries ={song}/>
+   </div>
+   )   
 }
 
 export default App;
---
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -0,0 +1,32 @@
+
+
+
+const DisplayMusic = (props) => {
+    return ( 
+        <table>
+        <thead>
+            <tr>
+                <th>Song title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Release Date</th>
+                <th>Genre</th>
+                <th>Likes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+        </table>
+    
+     );
+}
+ 
+export default DisplayMusic;
